759f8bdbfe2c70ce73c70954893c879d5a9f756c
giucarvalh0 <giuliano.carvalho@inatel.br>
2021-02-10 11:30:19 -0300

    adding chart and creatinh the 'dashboard-cuidador' component

--- frontend/src/app/dashboard-cuidador/dashboard-cuidador.component.ts
+++ frontend/src/app/dashboard-cuidador/dashboard-cuidador.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'dashboard-cuidador',
@@ -13,3 +14,20 @@ export class DashboardCuidadorComponent implements OnInit {
   }
 
 }
+$(document).ready(function () {
+
+  // $("#sidebar").mCustomScrollbar({
+  //      theme: "minimal"
+  // });
+
+  $('#sidebarCollapse').on('click', function () {
+      // open or close navbar
+      $('#sidebar').toggleClass('active');
+      // close dropdowns
+      $('.collapse.in').toggleClass('in');
+      // and also adjust aria-expanded attributes we use for the open/closed arrows
+      // in our CSS
+      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+
+});
--- frontend/src/app/graficos/graficos.component.ts
+++ frontend/src/app/graficos/graficos.component.ts
@@ -14,3 +14,29 @@ export class GraficosComponent implements OnInit {
   }
 
 }
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawBasic);
+
+function drawBasic() {
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'Hora');
+  data.addColumn('number', 'Umidade');
+  data.addRows([
+  [12, 30],  [13, 40],  [14, 50],  [15, 35],  [16, 50],  [17, 20],  [18, 25],  [19, 60]
+  ]);
+
+  var options = {
+    hAxis: {
+      title: 'Tempo (Hrs)'
+    },
+    vAxis: {
+      title: 'Umidade (%)'
+    },
+    colors:['#338326','#338326']
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
